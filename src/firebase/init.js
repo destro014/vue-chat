8c8b8bccc3b2fa25603669d3ca4ff4e70e91efea
destro014 <japmc11@gmail.com>
2019-09-10 21:04:51 +0545

    Initial Commit

---
+++ src/firebase/init.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp.firestore()
